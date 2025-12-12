18fc0ddb1bd780171b2a0bcd64eb1c76b936fc17
MossDog <lukejohnmosshughes@gmail.com>
2025-12-12 18:42:25 +0000

    Skeleton for Home Page

---
+++ components/TopBar.tsx
@@ -0,0 +1,9 @@
+export default function TopBar() {
+  return (
+    <header className="h-16 w-full flex items-center justify-center bg-base-300 backdrop-blur-md border-b-4 border-accent">
+      <h1 className="text-5xl font-bold tracking-wide text-base-content">
+        BAB
+      </h1>
+    </header>
+  );
+}
