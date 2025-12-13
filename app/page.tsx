4f16d05e277ee5a13562bf3e6a354a493b36c58b
MossDog <lukejohnmosshughes@gmail.com>
2025-12-13 19:59:21 +0000

    Polished bio formatting

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,15 @@ export default function Home() {
 
         <div className="flex flex-col gap-8 flex-1">
           <div className="card bg-blue-400">
-            <div className="card-body">
+            <div className="card-body space-y-4 prose prose-neutral max-w-none">
               <h2 className="card-title">Meet The Band</h2>
               <p>
                 Tuk Tuk Radio is the neighbourhood duo you didn’t know you needed in your life. Featuring Alexandru Baboi on guitar and Babis Katsaitis on bass, they set the mood and bring the vibes for a guaranteed good time.
-
+              </p>
+              <p>
                 While Tuk Tuk Radio usually performs in wine bars and restaurant settings, the duo is now aiming to take their sound to the stages of Dublin’s live music venues in the year ahead. Drawing influence from a wide range of genres, hence the name, you never quite know what to expect: it might be jazz, it might be funk, it might be chill, or it might be all of them at once.
-
+              </p>
+              <p>
                 Their goal is simple: to make people feel good and have a great time.
               </p>
             </div>
