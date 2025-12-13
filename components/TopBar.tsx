b3943fbbe57bcf0688436f10c7631f13c9da8209
MossDog <lukejohnmosshughes@gmail.com>
2025-12-13 22:50:52 +0000

    Thickened bottom border

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,6 +1,6 @@
 export default function TopBar() {
   return (
-    <header className=" sticky top-0 z-50 w-full bg-white py-4 border-b-2">
+    <header className="sticky top-0 z-50 w-full bg-white py-4 border-b-4">
       <div className="h-16 w-2/5 mx-auto flex items-center justify-center bg-blue-400 rounded-lg">
 
         <h1 className="text-4xl font-extrabold tracking-widest text-black [text-shadow:-1px_-1px_0_#FFF]">
