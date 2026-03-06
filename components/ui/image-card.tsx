7a89b633b1a19cd0836d23bf08dd0b7de99cd4ad
MossDog <lukejohnmosshughes@gmail.com>
2026-03-06 02:55:56 +0000

    Visual improvements

--- components/ui/image-card.tsx
+++ components/ui/image-card.tsx
@@ -11,7 +11,7 @@ export default function ImageCard({ imageUrl, caption, className }: Props) {
   return (
     <figure
       className={cn(
-        "w-[250px] overflow-hidden rounded-base border-2 border-border bg-main font-base shadow-shadow",
+        "w-[250px] overflow-hidden rounded-base bg-main font-base shadow-shadow border-2 border-border",
         className,
       )}
     >
@@ -22,7 +22,7 @@ export default function ImageCard({ imageUrl, caption, className }: Props) {
           src={imageUrl}
           alt={caption}
           fill
-          className="object-cover fill object-top"
+          className="object-cover object-top"
           sizes="250px"
         />
       </div>
